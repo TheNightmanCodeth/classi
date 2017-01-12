c8e75678f3831aa32e2674cf3b6c213d06e20f44
TheNightman <joyod3@gmail.com>
2017-01-11 19:46:17 -0500

    Implements edit class from class view

--- app/src/main/java/me/thenightmancodeth/classi/views/ClassView.java
+++ app/src/main/java/me/thenightmancodeth/classi/views/ClassView.java
@@ -1,28 +1,41 @@
 package me.thenightmancodeth.classi.views;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.List;
 
+import butterknife.BindColor;
 import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
 import io.realm.Realm;
+import io.realm.RealmList;
 import io.realm.RealmResults;
 import me.thenightmancodeth.classi.R;
 import me.thenightmancodeth.classi.models.data.Class;
+import me.thenightmancodeth.classi.models.data.Grade;
 
 public class ClassView extends AppCompatActivity {
     @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.fab) FloatingActionButton fab;
+    @BindColor(R.color.colorPrimary) int green;
+    @BindColor(R.color.colorAccent) int blue;
+
     @BindView(R.id.new_class_name)           EditText name;
     @BindView(R.id.new_class_prof)           EditText prof;
     @BindView(R.id.new_class_building)       EditText building;
@@ -48,14 +61,24 @@ public class ClassView extends AppCompatActivity {
         className = args.getString(MainActivity.CLASS_NAME_EXTRA);
         toolbar.setTitle(className);
         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter
+                .createFromResource(getApplicationContext(),
+                R.array.ampm, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromAMPMSpinner.setAdapter(spinnerAdapter);
+        toAMPMSpinner.setAdapter(spinnerAdapter);
+        fromAMPMSpinner.setEnabled(false);
+        toAMPMSpinner.setEnabled(false);
 
         Realm.init(getApplicationContext());
-        Realm realm = Realm.getDefaultInstance();
+        final Realm realm = Realm.getDefaultInstance();
 
-        RealmResults<Class> thisClassFromRealm = realm.where(Class.class)
-                .equalTo("name", className).findAll();
-        thisClass = thisClassFromRealm.get(0);
-
+        thisClass = realm.where(Class.class).equalTo("name", className).findFirst();
         if (thisClass != null) {
             name.setText(thisClass.getName());
             prof.setText(thisClass.getProfessor());
@@ -64,7 +87,7 @@ public class ClassView extends AppCompatActivity {
             fromMin.setText(String.valueOf(thisClass.getTimeFromM()));
             toHour.setText(String.valueOf(thisClass.getTimeToH()));
             toMin.setText(String.valueOf(thisClass.getTimeToM()));
-            fromAMPMSpinner.setSelection(ClassRecycleAdapter.amPmIntFrom(thisClass.getFromAMPM()) +1);
+            fromAMPMSpinner.setSelection(ClassRecycleAdapter.amPmIntFrom(thisClass.getFromAMPM()));
             Log.i("int", "" +ClassRecycleAdapter.amPmIntFrom(thisClass.getFromAMPM()));
             toAMPMSpinner.setSelection(ClassRecycleAdapter.amPmIntFrom(thisClass.getToAMPM()));
             for (CheckBox c : checkBoxes) {
@@ -74,12 +97,58 @@ public class ClassView extends AppCompatActivity {
             }
         }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Animation spin = new RotateAnimation(0.0f, 360.0f,
+                        fab.getWidth()/2, fab.getHeight()/2);
+                spin.setRepeatCount(5);
+                spin.setDuration(10);
+
+                if (!name.isEnabled()) {
+                    fab.setImageResource(R.drawable.ic_check_24dp);
+                    fab.setBackgroundTintList(ColorStateList.valueOf(green));
+                } else {
+                    fab.setImageResource(R.drawable.ic_edit_24dp);
+                    fab.setBackgroundTintList(ColorStateList.valueOf(blue));
+
+                    //Get data from input
+                    String nameString = name.getText().toString();
+                    String profString = prof.getText().toString();
+                    String builString = building.getText().toString();
+                    String days = "";
+                    for (CheckBox c : checkBoxes) {
+                        if (c.isChecked()) {
+                            days += c.getText().toString();
+                        }
+                    }
+
+                    //Edit object in realm
+                    realm.beginTransaction();
+                    thisClass.setBuilding(builString);
+                    thisClass.setDays(days);
+                    thisClass.setName(nameString);
+                    thisClass.setProfessor(profString);
+                    thisClass.setTimeFromH(Integer.valueOf(fromHour.getText().toString()));
+                    thisClass.setTimeFromM(Integer.valueOf(fromMin.getText().toString()));
+                    thisClass.setFromAMPM(fromAMPMSpinner.getSelectedItem().toString());
+                    thisClass.setTimeToH(Integer.valueOf(toHour.getText().toString()));
+                    thisClass.setTimeToM(Integer.valueOf(toMin.getText().toString()));
+                    thisClass.setToAMPM(toAMPMSpinner.getSelectedItem().toString());                    
+                    realm.commitTransaction();
+                }
+                name.setEnabled(!name.isEnabled());
+                prof.setEnabled(!prof.isEnabled());
+                building.setEnabled(!building.isEnabled());
+                fromHour.setEnabled(!fromHour.isEnabled());
+                fromMin.setEnabled(!fromMin.isEnabled());
+                toHour.setEnabled(!toHour.isEnabled());
+                toMin.setEnabled(!toMin.isEnabled());
+                fromAMPMSpinner.setEnabled(!fromAMPMSpinner.isEnabled());
+                toAMPMSpinner.setEnabled(!toAMPMSpinner.isEnabled());
+                for (CheckBox c : checkBoxes) {
+                    c.setEnabled(!c.isEnabled());
+                }
             }
         });
     }
